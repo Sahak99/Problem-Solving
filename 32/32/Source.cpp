32730c2570e84cfb90e393653d1431b5a8485593
Sahak99 <sahakmatevosyan1999@gmail.com>
2021-02-14 16:58:38 +0400

    32

--- 32/32/Source.cpp
+++ 32/32/Source.cpp
@@ -1,27 +1,54 @@
 #include <iostream>;
 using namespace std;
 void findSequence(char*);
+void setAlphabet(char*);
 
 int main() {
 	char str[30];
 	cout << "Enter a string: ";
-	cin >> str;
+	cin.getline(str,30);
 	findSequence(str);
 }
 
 void findSequence(char* str) {
-	int length = 0;
-	int seqInd = 0;
-	int maxSeq = 0;
-	while (str[length]) length++;
-	int* seq = new int[length];
-	for (int i = 0; i < length; i++)
-		if ((str[i] == str[i + 1]) || (str[i - 1] == str[i])) {
-			seq[seqInd] = i;
-			seqInd++;
+	int str_length = 0;
+	char alphabet[26];
+	int alphabet_values[26];
+	for (int i = 0; i < 26; i++) {
+		alphabet_values[i] = 1;
+	}
+	setAlphabet(alphabet);
+	while (str[str_length]) {
+		str[str_length] = tolower(str[str_length]);
+		str_length++; 
+	}
+	char temp = str[0];
+	for (int i = 1; i < str_length; i++) {
+		if (temp == str[i]) {
+			for (int j = 0; j < 26; j++)
+				if (alphabet[j] == str[i]) {
+					alphabet_values[j]++;
+					break;
+				}
 		}
+		else temp = str[i];
+	}
+	int max = 1;
+	int maxIndex = 0;
+	for (int i = 0; i < 26; i++) {
+		if (max < alphabet_values[i]) {
+			max = alphabet_values[i];
+			maxIndex = i;
+		}
+	}
 	
+	cout << "Max:" << alphabet[maxIndex] << ", " << max << " times.";
 }
 
 
 
+void setAlphabet(char* alphabet) {
+	alphabet[0] = 'a';
+	for (int i = 1; i < 26; i++)
+		alphabet[i] = (char)(alphabet[i - 1] + 1);
+}
